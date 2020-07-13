49288f466b775846985348e900b798cfa38055e9
fairnia <hello@fairnia.com>
2020-07-12 20:07:56 -0700

    removed chatbox code

--- public/chatclient.js
+++ public/chatclient.js
@@ -111,17 +111,11 @@ function connect() {
   log(`Connecting to server: ${serverUrl}`);
   connection = new WebSocket(serverUrl, "json");
 
-  connection.onopen = function(evt) {
-    document.getElementById("text").disabled = false;
-    document.getElementById("send").disabled = false;
-  };
-
   connection.onerror = function(evt) {
     console.dir(evt);
   }
 
   connection.onmessage = function(evt) {
-    var chatBox = document.querySelector(".chatbox");
     var text = "";
     var msg = JSON.parse(evt.data);
     log("Message received: ");
@@ -132,15 +126,10 @@ function connect() {
     switch(msg.type) {
       case "id":
         clientID = msg.id;
-        setUsername();
         break;
 
       case "username":
         text = "<b>User <em>" + msg.name + "</em> signed in at " + timeStr + "</b><br>";
-        break;
-
-      case "message":
-        text = "(" + timeStr + ") <b>" + msg.name + "</b>: " + msg.text + "<br>";
         break;
 
       case "rejectusername":
@@ -184,14 +173,6 @@ function connect() {
         console.log('unknown message on refresh');
         console.dir(msg, { depth: null });
         log_error(msg);
-    }
-
-    // If there's text to insert into the chat buffer, do so now, then
-    // scroll the chat panel so that the new text is visible.
-
-    if (text.length) {
-      chatBox.innerHTML += text;
-      chatBox.scrollTop = chatBox.scrollHeight - chatBox.clientHeight;
     }
   };
 }
